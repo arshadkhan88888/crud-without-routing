0615c7e8b5900c69661455e790914065cbfbb3dc
arshad khan <arshadkhan88888>
2021-04-12 15:14:55 +0530

    final the project finalize

--- src/Routing/Professional_detail.js
+++ src/Routing/Professional_detail.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link} from "react-router-dom";
-import ReactShareSocial from "react-share-social";
+import { FacebookButton, FacebookCount, LinkedInButton, TwitterButton } from "react-social";
+import {FacebookIcon, LinkedinIcon, TwitterIcon} from 'react-share'
 
 const Professional_detail = (props) => {
   const [data1, setData1] = useState([]);
   const [data2, setData2] = useState([]);
 
-  let url=`https://alkurntech-reactjs.herokuapp.com/social/?slug=${props.match.params.id}`
+  let url=`https://alkurntech-reactjs.herokuapp.com/social/${props.match.params.id}`
 
   useEffect(() => {
     axios
@@ -53,10 +54,16 @@ const Professional_detail = (props) => {
             <h6>{data2.subject_names}</h6>
           </div>
         </div>
-        <ReactShareSocial
-                    url={url}
-                    socialTypes={["facebook", "twitter","linkedin"]}
-                  />
+        <div className='col-md-4' style={{bottom:'14px'}}>
+        <FacebookButton  url={url} appId={264765292010240}>
+          <FacebookIcon size='35' round={true}/>
+          </FacebookButton>
+          
+         <LinkedInButton url={url} Client ID={'864k3totrdnv64'}>
+          <LinkedinIcon size='35' round={true}/>
+      </LinkedInButton>
+
+        </div>
         <Link to="/" className="btn btn-warning">
           Go Back
         </Link>
