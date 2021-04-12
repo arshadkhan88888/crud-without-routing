2dc0072b71d64ff778ed28ea9baee3aa6de8dfde
arshad khan <arshadkhan88888>
2021-04-12 15:27:11 +0530

    final the project finalize

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import App from './App';
-// import Professional from './Professional';
 
 ReactDOM.render(
   <App />
-  // <Professional/>
+  
 ,document.getElementById('root'));
