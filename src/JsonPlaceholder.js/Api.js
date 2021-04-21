6934ea9e26db15aaca2c67e21c9996f4956889f9
arshad khan <arshadkhan88888>
2021-04-22 03:32:00 +0530

    Fake APi For Social Sharing

--- src/JsonPlaceholder.js/Api.js
+++ src/JsonPlaceholder.js/Api.js
@@ -1,63 +1,53 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import { FacebookIcon } from 'react-share';
-import {FacebookButton} from "react-social";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import { FacebookIcon } from "react-share";
+import { FacebookButton } from "react-social";
 
 const Api = () => {
+  const [list, setList] = useState([]);
 
-    const [list, setList] = useState([])
-    
-    let url ='https://fake-api-placeholder.herokuapp.com/'
+  let url = "https://fake-api-placeholder.herokuapp.com/";
 
-    useEffect(()=> {
-        restApi()
-    },[list])
-    
-    const restApi = () => {
-        axios.get('https://jsonplaceholder.typicode.com/photos')
-        .then((res)=>{setList(res.data)})
-    }
+  useEffect(() => {
+     axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => {
+        setList(res.data);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h1>Json PlaceHolder</h1>
-            
-            <table className='table table-bordered table-info'>
-                <thead>
-                <tr>
-                <th>Album id</th>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Url</th>
-                <th>Image</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        list.map((ele) =>
-                            
-                            <tr>
-                                <td>{ele.albumId}</td>
-                                <td>{ele.id}</td>
-                                <td>{ele.title}</td>
-                                <td>{ele.url}</td>
-                                <td><img src={ele.thumbnailUrl} /></td>
-                                <td> <FacebookButton url={url} appId={264765292010240}>
-                                <FacebookIcon size="35" round={true} />
-                                </FacebookButton></td>
-                                <Helmet>
-                                <title>{ ele.title}</title>
-                                <meta property="og:title" content={ele.title}/>
-                                <meta property="og:image" content={ele.thumbnailUrl}/>
-                                </Helmet>
-                            </tr>
-                        )
-                }
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Json PlaceHolder</h1>
+
+      <table className="table table-bordered table-info">
+        <thead>
+          <tr>
+            <th>Album id</th>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Url</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((ele) => (
+            <tr>
+              <td>{ele['albumId']}</td>
+              <td>{ele['id']}</td>
+              <td>{ele['title']}</td>
+              <td>{ele['url']}</td>
+              <td>
+                <img src={ele['thumbnailUrl']} />
+                  </td>
+                  
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Api;
