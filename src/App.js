8a1af9a22f77c0a7f983cf75e3b686ca4267e8fc
arshadkhan88888 <arshad.khan88888.com>
2021-05-10 21:58:11 +0530

    task From Qloron Crud Without Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
-import Add_User from './Context/Add_User';
-import { ContxtProvider } from './Context/ContextGlobal';
-import Users from './Context/Users';
-import Edit_User from './Context/Edit_User';
+// import React from 'react';
+// import {BrowserRouter as Router, Route } from 'react-router-dom';
+// import Add_User from './Context/Add_User';
+// import { ContxtProvider } from './Context/ContextGlobal';
+// import Users from './Context/Users';
+// import Edit_User from './Context/Edit_User';
 
 
-const App = () => {
-  return (
-    <ContxtProvider>
-    <div className="App">
-    <h1>Crud Operation</h1>
-    <Router>
-    <Route exact path='/' component={Users}/>
-    <Route path='/add_user' component={Add_User}/>
-    <Route path="/edit_user/:id" component={Edit_User}/>
-    </Router>
-  </div>
-  </ContxtProvider>
-  );
-};
+// const App = () => {
+//   return (
+//     <ContxtProvider>
+//     <div className="App">
+//     <h1>Crud Operation</h1>
+//     <Router>
+//     <Route exact path='/' component={Users}/>
+//     <Route path='/add_user' component={Add_User}/>
+//     <Route path="/edit_user/:id" component={Edit_User}/>
+//     </Router>
+//   </div>
+//   </ContxtProvider>
+//   );
+// };
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import CrudwithHooks from './Basic_Crud_with_hooks/CrudO[peration';
-// import App from './App';
 
 ReactDOM.render(
-  // <App />
 <CrudwithHooks/>
  ,document.getElementById('root'));
